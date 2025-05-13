1baf120baa576f14cf2e4c62ae4e33a4dbcb295d
ramram419 <barbara0419@naver.com>
2025-05-13 09:22:11 +0900

    first commit

---
+++ src/components/cardComponent.tsx
@@ -0,0 +1,15 @@
+const Card = () => {
+  return (
+    <div>
+      <div>img</div>
+      <div>title</div>
+      <div>질문</div>
+      <div>
+        <div>adf</div>
+        <div> qjmss</div>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
---
+++ src/pages/surveys/index.tsx
@@ -0,0 +1,10 @@
+import Card from "@/components/cardComponent";
+
+export default function SurveyHome() {
+  return (
+    <div className="w-[276px]">
+      <Card />
+      <Card />
+    </div>
+  );
+}
